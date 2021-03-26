ec059361a864f5d3cca9e193ad0e6b8ceae557df
Jonathan RIVO <rivo.jonathan98@gmail.com>
2021-03-26 23:24:17 +0100

    Installed PHPUnit in project, started writing tests but need fix

--- app/tests/ItemTest.php
+++ app/tests/ItemTest.php
@@ -1,10 +1,22 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+
+require '../src/Utils/ItemList.php';
+
 class ItemTest extends TestCase
 {
+    private ItemList $item;
+
     protected function setUp(): void
     {
-        $this->item = new Item();
+        $this->item = new ItemList('new', 'this is a test', date_create()->format('Y-m-d H:i:s'));
         parent::setUp();
     }
+
+    public function isValidNominal()
+    {
+        $this->assertTrue($this->item->isValid());
+    }
+
 }
