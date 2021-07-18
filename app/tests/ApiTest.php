6a63378336af3d4fd6a07d6a8e4687d22a232862
Jonathan RIVO <rivo.jonathan98@gmail.com>
2021-07-18 23:09:36 +0200

    Added new tests, need to fix 1 route

--- app/tests/ApiTest.php
+++ app/tests/ApiTest.php
@@ -26,7 +26,7 @@ class ApiTest extends TestCase
     public function testDeleteToDoList()
     {
         $data = array(
-            "id"=>"22",
+            "id"=>"25",
         );
         $response = $this->client->post("/api/delete-todo-list", array(
             'form_params' => $data));
@@ -61,4 +61,99 @@ class ApiTest extends TestCase
         $expected = json_encode(array("result"=>"todo list created"));
         $this->assertEquals($expected,$body);
     }
+
+    public function testAddItemTodoList()
+    {
+        $data = array(
+            "todolist_id"=>"1",
+            "item_name"=>"newItem",
+            "item_content"=>"newItemContent",
+            "creation_date"=>(new \DateTime)->format('d/m/Y H:i:s'),
+        );
+        $response = $this->client->post("/api/add-todo-item", array(
+            'form_params' => $data));
+        $this->assertEquals(200,$response->getStatusCode());
+        $body = $response->getBody();
+        $expected = json_encode(array("result" => "item successfully added"));
+        $this->assertEquals($expected,$body);
+    }
+
+    public function testAddItemTodoListWithoutId()
+    {
+        $data = array(
+            "item_name"=>"newItem",
+            "item_content"=>"newItemContent",
+            "creation_date"=>(new \DateTime)->format('d/m/Y H:i:s'),
+        );
+        $response = $this->client->post("/api/add-todo-item", array(
+            'form_params' => $data));
+        $this->assertEquals(200,$response->getStatusCode());
+        $body = $response->getBody();
+        $expected = json_encode(array("result"=>"todo list id not specified"));
+        $this->assertEquals($expected,$body);
+    }
+
+    public function testAddItemTodoListThatNotExist()
+    {
+        $data = array(
+            "todolist_id"=>"10000",
+            "item_name"=>"newItem",
+            "item_content"=>"newItemContent",
+            "creation_date"=>(new \DateTime)->format('d/m/Y H:i:s'),
+        );
+        $response = $this->client->post("/api/add-todo-item", array(
+            'form_params' => $data));
+        $this->assertEquals(200,$response->getStatusCode());
+        $body = $response->getBody();
+        $expected = json_encode(array("result" => "todo list not found"));
+        $this->assertEquals($expected,$body);
+    }
+
+    public function testAddExistingItemTodoList()
+    {
+        $data = array(
+            "todolist_id"=>"1",
+            "item_name"=>"newItem",
+            "item_content"=>"newItemContent",
+            "creation_date"=>(new \DateTime)->format('d/m/Y H:i:s'),
+        );
+        $response = $this->client->post("/api/add-todo-item", array(
+            'form_params' => $data));
+        $this->assertEquals(200,$response->getStatusCode());
+        $body = $response->getBody();
+        $expected = json_encode(array("result" => "item name already exists"));
+        $this->assertEquals($expected,$body);
+    }
+
+    public function testAddItemTodoListTooLong()
+    {
+        $data = array(
+            "todolist_id"=>"1",
+            "item_name"=>"newItem",
+            "item_content"=>str_repeat("a",1200),
+            "creation_date"=>(new \DateTime)->format('d/m/Y H:i:s'),
+        );
+        $response = $this->client->post("/api/add-todo-item", array(
+            'form_params' => $data));
+        $this->assertEquals(200,$response->getStatusCode());
+        $body = $response->getBody();
+        $expected = json_encode(array("result" => "Max characters for the content is 1000"));
+        $this->assertEquals($expected,$body);
+    }
+
+    /*public function testAddItemTodoListTooEarly()
+    {
+        $data = array(
+            "todolist_id"=>"1",
+            "item_name"=>"newItem",
+            "item_content"=>"newItemContent",
+            "creation_date"=>(new \DateTime)->format('d/m/Y H:i:s'),
+        );
+        $response = $this->client->post("/api/add-todo-item", array(
+            'form_params' => $data));
+        $this->assertEquals(200,$response->getStatusCode());
+        $body = $response->getBody();
+        $expected = json_encode(array("result" => "You have to wait 30 minutes every time you create an item"));
+        $this->assertEquals($expected,$body);
+    }*/
 }
